72ee3930de2aaf9c4f079520c257be6d3c4741f7
Andrius <>
2020-08-06 21:31:22 +0100

    very scecific - just another not so great looking designfixed :)

--- static/js/js.js
+++ static/js/js.js
@@ -33,3 +33,23 @@
         document.documentElement.scrollTop = 0;
       }
     }
+
+
+    /*Tab function for account page, code from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_tabs_open*/
+
+    function openTab(evt, tabName) {
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
+      document.getElementById(tabName).style.display = "block";
+      evt.currentTarget.className += " active";
+    }
+    
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
